2486833c1988fb30c300aa66ba07bbdbb1840a43
~elma-yo~ <~elmaspy@gmail.com~>
2022-09-12 23:13:23 +0900

    08. Todoクラスを完成する

--- todo_app_php_v4-master/work/public/index.php
+++ todo_app_php_v4-master/work/public/index.php
@@ -2,13 +2,11 @@
 
 require_once(__DIR__ . '/../app/config.php');
 
-Token::create();
-
 $pdo = Database::getInstance();
 
 $todo = new Todo($pdo);
 $todo->processPost();
-$todo = $todo->getAll();
+$todos = $todo->getAll();
 
 ?>
 <!DOCTYPE html>
